12d7cd0b38fec6e20c5ca3530447a96f2a0724e5
shahviraj222 <viraj22feb@gmail.com>
2024-12-08 17:14:08 +0530

    insertion sort

---
+++ 04sorting/insertionSort.py
@@ -0,0 +1,18 @@
+"""
+Insertion sort usefull for the link list O(n)
+For Array Insertion Sort O(n^2)
+"""
+
+a = [12,233,34,3] 
+for i in range(1,len(a)):
+    key = a[i]
+    j = i-1
+    # swift if the key is grater then element to be inserted 
+    while j >= 0 and a[j]>key:
+        a[j+1] = a[j]
+        j-=1
+    
+    a[j+1] = key
+
+
+print(a)
