5b4146a16b09db12ac76e2e7394c51dd28f2ea9a
shahviraj222 <viraj22feb@gmail.com>
2025-02-24 22:08:28 +0530

    error find

--- 05problem.py/02arrayIsSorted.py
+++ 05problem.py/02arrayIsSorted.py
@@ -19,14 +19,14 @@ def removeduplicate(arr):
 
     return len(mySet)     
 
-# method 2
+# method 2 error
 def removeduplicate2(arr):
     i = 0
     for j in range(1,len(arr)):
         if arr[j] != arr[i]:
             arr[i+1] = arr[j]
             i +=1
-    print(arr)        
+    print(arr)         
     return i+1
 
 
