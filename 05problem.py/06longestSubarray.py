20a1ad99e415191576fd2614b73f0c9d7864fdc2
shahviraj222 <viraj22feb@gmail.com>
2025-02-26 17:37:40 +0530

    new problem added

--- 05problem.py/06longestSubarray.py
+++ 05problem.py/06longestSubarray.py
@@ -12,4 +12,41 @@ def findLongestSubarray(arr,s):
 
     return lenth+1
 
-print(findLongestSubarray([1,1,1,1,34,45,54,22],4))
+def findLongestSubarray2(arr,s):
+    length = 0
+    for i in range(0,len(arr)):
+        sum = 0
+        for j in range(i,len(arr)):
+            sum+=arr[j]
+            if sum == s:
+                length = max(length,j-i+1)
+
+    return length             
+
+print(findLongestSubarray2([1,1,1,1,34,45,54,22],4))
+
+# Problem2 : Find the index of two nums to get target sum
+
+# methode 1:BruteForce
+def targetTwo(arr,target):
+    for i in range(0,len(arr)):
+        for j in range(i,len(arr)):
+            if target == arr[i]+arr[j]:
+                return [i,j]
+
+
+print(targetTwo([1,32,3,9],12))
+
+# methode 2: Hashmap
+def targetTwo2(arr,target):
+    map = {}
+    for i in range(0,len(arr)):
+        remaining = target-arr[i]
+        if remaining in map:
+            return [i,map[remaining]]
+        else:
+            map[arr[i]] = i
+
+print(targetTwo2([1,32,3,9],12))
+# Time Complexity is O(n)
+# Space Complexity is O(n)
--- 05problem.py/tempCodeRunnerFile.py
+++ 05problem.py/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
- 
+def targetTwo(arr,target):
+    map = {}
+    for i in range(0,len(arr)):
+        remaining = target-arr[i]
+        if remaining in map:
+            return [i,map[remaining]]
+        else:
+            map[arr[i]] = i
+
+print(targetTwo([1,32,3,9],12))
