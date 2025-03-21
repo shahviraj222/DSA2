6e42a5db41c71cd56388b5e157fb3cd527031e1a
shahviraj222 <viraj22feb@gmail.com>
2025-03-21 16:42:03 +0530

    problem updated

--- Array Problems/22MergeSortedArray.py
+++ Array Problems/22MergeSortedArray.py
@@ -43,13 +43,15 @@ def merge2(arr1,arr2,n,m):
     while left>=0 and right<m:
         if arr1[left]>arr2[right]:
             arr1[left],arr2[right] =  arr2[right],arr1[left]
+            left-=1
+            right+=1
         else:
             break
 
-    arr1.sort()        
-    arr2.sort()
+    arr1[n:] =arr2
+    arr1.sort() 
     
-    print(arr1,arr2)
+    print(arr1)
 
 merge2([1,2,3],[2,5,6],3,3)  
 
