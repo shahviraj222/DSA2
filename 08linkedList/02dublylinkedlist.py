6c01f56a80720b690045bd891d8de48e931b8707
shahviraj222 <viraj22feb@gmail.com>
2025-05-01 21:36:39 +0530

    new problem added

---
+++ 08linkedList/02dublylinkedlist.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+       self.head = None
+
+    def append(self):
+        pass
+
+    def prepend(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def printForward(self):
+        pass
+
+    def printBackward(self):
+        pass    
