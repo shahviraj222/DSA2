e0a76fed35cc4f42763ee1c9bf4740ccdd5a7fc1
shahviraj222 <viraj22feb@gmail.com>
2025-05-12 21:34:05 +0530

    added new problem

---
+++ 08linkedList/06oddEven.py
@@ -0,0 +1,6 @@
+# problem : seprate odd and even linkedlist 
+
+# method 1: using temporary array.
+# go odd and even linkedlist and store the data into to the temporary array and the  copy back to linkedlist
+
+# method 2 : Using link transfering with two pointer odd and even
