65df5a38e4d6c6f445c8672266d5bd8809d373a2
shahviraj222 <viraj22feb@gmail.com>
2025-05-27 12:38:09 +0530

    new problem added

---
+++ 08linkedList/07findIntersection.py
@@ -0,0 +1,9 @@
+# problem : two linked list is given find the first intersection point
+
+# method1: Add everynode of first linked list in the dictionary and search for second  list
+# if you find the mathch return true
+
+# method2: count the node in the both linked list and the find the difference between them and move the pointer until both stand on similar index
+
+# method3: Using two pointer
+
---
+++ 08linkedList/08Findpair.py
@@ -0,0 +1,31 @@
+# Problem : find the pair that gives sum equal to given in sorted doubly linked list 
+
+# methode 1: using nested loops
+
+temp1 = head
+ds = []
+while temp1:
+    temp2 = temp1.next
+    while temp2:
+        if temp1.value + temp2.value == Key:
+            ds.add(temp1.value,temp2.value)
+        temp2 = temp2.next    
+    temp1 = temp1.next
+
+
+# methode 2: using two pointer left and right
+
+left = head
+right = tail of linked list
+
+while left.value < right.value:
+    if left.value + right.value == sum:
+        ds.add(pair)
+        left = left.next
+        right = right.prev
+    elif left.value + right.value>sum:    
+        right = right.prev 
+    else:
+        left = left.next
+        
+ 
