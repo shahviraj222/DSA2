36cf91abc03f0fa1ec27b6980a69c253bf4125b2
shahviraj222 <viraj22feb@gmail.com>
2026-04-14 15:25:51 +0530

    adding problem

--- temp.py
+++ temp.py
@@ -1,35 +1,55 @@
+# remove duplicate from sorted array
+
+def removduplicate(arr):
+    j = 1
+    i = 0
+    while j<len(arr):
+        while j < len(arr) and arr[i]==arr[j]:
+            j+=1
+        
+        if j >=len(arr):
+            break
+
+        i+=1
+        arr[i]=arr[j]    
+        j+=1
+
+    for k in range(0,i+1):
+        print(arr[k])
+
+removduplicate([1,1,1,1,1,1,1,1,2,2,2,2,2])
 
 
 # sorting
 
-def partition(arr, l, h):
-    i = l + 1
-    j = h
+# def partition(arr, l, h):
+#     i = l + 1
+#     j = h
 
-    while True:
-        while i <= j and arr[i] <= arr[l]:
-            i += 1
-        while i <= j and arr[j] > arr[l]:
-            j -= 1
+#     while True:
+#         while i <= j and arr[i] <= arr[l]:
+#             i += 1
+#         while i <= j and arr[j] > arr[l]:
+#             j -= 1
         
-        if i <= j:
-            arr[i], arr[j] = arr[j], arr[i]
-        else:
-            break
+#         if i <= j:
+#             arr[i], arr[j] = arr[j], arr[i]
+#         else:
+#             break
 
-    arr[l], arr[j] = arr[j], arr[l]
-    return j
-
-
-def quicksort(arr, l, h):
-    if l < h:
-        p = partition(arr, l, h)
-        quicksort(arr, l, p - 1)
-        quicksort(arr, p + 1, h)
-    return arr
+#     arr[l], arr[j] = arr[j], arr[l]
+#     return j
 
 
-print(quicksort([2, 3, 1, 0], 0, 3))
+# def quicksort(arr, l, h):
+#     if l < h:
+#         p = partition(arr, l, h)
+#         quicksort(arr, l, p - 1)
+#         quicksort(arr, p + 1, h)
+#     return arr
+
+
+# print(quicksort([2, 3, 1, 0], 0, 3))
         
 
 
