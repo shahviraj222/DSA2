2f711ce8299880b370f29586c60016c2bba060b1
shahviraj222 <viraj22feb@gmail.com>
2026-04-19 20:04:59 +0530

    first digit removal conept is wrong

--- 01BasicProblems/01countDigit.py
+++ 01BasicProblems/01countDigit.py
@@ -2,7 +2,9 @@
 # num // 10 → remove last digit 
 
 # num // (10^k) → first digit 
-# num % (10^k) → remove first digit
+# num % (10^k) → remove first digit  !!!!problem!!!
+#  we have problem here suppose number is 1012 then when we remove first digit it will give 12 because when you do math 012 becomes 12 (as we see as math)
+
 
 # k is total digits on right side form first element = (total elemnt - 1)
 
