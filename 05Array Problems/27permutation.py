03baba6ec01cc6faa5e9e1b4fda3ef886b82d295
shahviraj222 <viraj22feb@gmail.com>
2026-04-27 22:33:55 +0530

    permutation is added

--- 05Array Problems/27permutation.py
+++ 05Array Problems/27permutation.py
@@ -9,5 +9,32 @@
 # Time complexity is O(n!)
 
 
-# problem 2 : find the next number in array
+# problem 2 : find the next number in array according to dictionary order
 # https://www.youtube.com/watch?v=JDOXKqF60RQ
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        index = -1
+        # find index longest prefix
+        for i in range(len(nums)-1):
+            if nums[i]<nums[i+1]:
+                index = i
+        # find the bigger the ith but smallest
+        min = float("inf")
+        minindex = -1
+        for i in range(index,len(nums)):
+            if nums[i]>nums[index]:
+                if min > nums[i]:
+                    min = nums[i]
+                    minindex = i
+
+        # swap elements
+        nums[minindex],nums[index] = nums[index], nums[minindex]
+        
+        # sort the element from i+1 index to the len(nums)
+        nums[index+1:] = sorted(nums[index+1:])
+
+
+
+        
